31168f3f769938df91121c0633c02d1f73ee546a
eclancy68 <eclancy68@gmail.com>
2016-05-19 19:45:50 -0400

    Added movement and jumping

--- Assets/Scripts/PlayerStats.js
+++ Assets/Scripts/PlayerStats.js
@@ -1,14 +1,29 @@
-﻿#pragma strict
-
+﻿
 var health = 3;
 var swingDelay = 1;
-var jumpStrength = 3;
-var moveSpeed = 3;
+var jumpStrength: int = 10;
+var moveSpeed = 5;
+var isGrounded;
+var rb: Rigidbody2D;
+
+var groundCheck: Transform;
+var groundCheckRadius = .1;
+var whatIsGround: LayerMask;
+var grounded: System.Boolean;
+
+function FixedUpdate(){
+    isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+}
 
 function Start () {
-
+    rb = GetComponent.<Rigidbody2D>();
 }
 
 function Update () {
+    if(Input.GetButtonDown("A_1") && isGrounded){
+        rb.velocity.y = jumpStrength;
+    }
 
+    rb.velocity.x = moveSpeed * Input.GetAxis("L_XAxis_1");
+    
 }
